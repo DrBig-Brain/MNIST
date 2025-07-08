ac34a88da862008835773a1e6cde1a07f4a0ecdb
Abhinav Mishra <abhinav714b@gmail.com>
2025-07-09 00:30:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,28 @@ import numpy as np
 
 try:
     model = load_model('model.keras')
+    st.title("CNN number predictor")
+
+    canvas_result = st_canvas(
+        stroke_width=10,
+        stroke_color="white",
+        background_color="black",
+        width=280,
+        height=280,
+        drawing_mode="freedraw",
+        key="canvas"
+        )
+
+    if canvas_result.image_data is not None:
+        img = Image.fromarray((canvas_result.image_data[:,:,0:3]))
+        img = img.convert("L")
+        img = img.resize((28,28)) 
+        img_arr = np.array(img).astype('float32')
+        img_arr = img_arr.reshape((1,28,28,1))
+        if st.button('Predict'):
+            pred = model.predict(img_arr)
+            st.success(f"Prediction : {np.argmax(pred)}")
+            st.bar_chart(pred[0])
 except:
     st.write("model failed to load, try re-running the application")
-st.title("CNN number predictor")
 
-canvas_result = st_canvas(
-    stroke_width=10,
-    stroke_color="white",
-    background_color="black",
-    width=280,
-    height=280,
-    drawing_mode="freedraw",
-    key="canvas"
-)
-
-if canvas_result.image_data is not None:
-    img = Image.fromarray((canvas_result.image_data[:,:,0:3]))
-    img = img.convert("L")
-    img = img.resize((28,28)) 
-    img_arr = np.array(img).astype('float32')
-    img_arr = img_arr.reshape((1,28,28,1))
-    if st.button('Predict'):
-        pred = model.predict(img_arr)
-        st.success(f"Prediction : {np.argmax(pred)}")
-        st.bar_chart(pred[0])
